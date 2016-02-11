477f262c77381fb644cac3ab926f2f72d1ffc896
SOPHIA KEMBLE <METOLIUS4@GMAIL.COM>
2016-02-11 10:53:37 -0500

    Second Styling Session, In Class

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210140748) do
+ActiveRecord::Schema.define(version: 20160211151737) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160210140748) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "artist_id"
+    t.string   "video"
   end
 
 end
